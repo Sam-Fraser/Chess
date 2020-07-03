7d42836508b7a1b6fb6387412834344ea06a6633
Sam Eff <HeySamEff@gmail.com>
2020-07-03 14:47:34 -0700

    finished create_board method

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,29 @@
 #lib/board.rb
+require_relative "symbols.rb"
+
+#board class for all functions related to the gameboard
+class Board
+  include Symbols
+  attr_accessor :board
+
+  #initialize board
+  def initialize
+    @board = starting_board
+  end
+
+  #create starting board
+  def starting_board
+    board = create_board
+    board.each do |arr|
+      arr.each
+    end
+  end
+
+  #create board
+  def create_board
+    board = Array.new(8).map do |x|
+      Array.new(8, {base: "", piece: ""})
+    end
+    board
+  end
+end
