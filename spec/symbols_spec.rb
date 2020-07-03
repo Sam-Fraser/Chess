a41f015341c6d8f2baa84b265ccf47b9ca7fa531
Sam Eff <HeySamEff@gmail.com>
2020-07-03 14:20:43 -0700

    created test for get_symbol method in Symbols module

---
+++ spec/symbols_spec.rb
@@ -0,0 +1,18 @@
+#spec/symbols_spec.rb
+require "./lib/symbols.rb"
+
+describe Symbols do
+  describe "#get_symbol" do
+    class DummyClass
+    end
+
+    before(:each) do
+      @dummy_class = DummyClass.new
+      @dummy_class.extend(Symbols)
+    end
+
+    it "will return the piece asked for" do
+      expect(@dummy_class.get_symbol("w_king")).to eql "\u{2654}"
+    end
+  end
+end
