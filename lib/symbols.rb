62ee3e102e50a584959e6e824c99ae949b7df77d
Sam Eff <HeySamEff@gmail.com>
2020-07-03 13:49:53 -0700

    created symbols hash

---
+++ lib/symbols.rb
@@ -0,0 +1,21 @@
+#lib/symbols.rb
+
+module Symbols
+  symbols = {
+    w_board: "\u{25A1}",
+    w_king: "\u{2654}",
+    w_queen: "\u{2655}",
+    w_rook: "\u{2656}",
+    w_bishop: "\u{2657}",
+    w_knight: "\u{2658}",
+    w_pawn: "\u{2659}",
+    b_board: "\u{25A0}",
+    b_king: "\u{265A}",
+    b_queen: "\u{265B}",
+    b_rook: "\u{265C}",
+    b_bishop: "\u{265D}",
+    b_knight: "\u{265E}",
+    b_pawn: "\u{265F}"
+  }
+  
+end
