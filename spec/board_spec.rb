83c9cacf1599a46e2b4d7c7394ce886fdf8c35ec
Sam Eff <HeySamEff@gmail.com>
2020-07-03 14:48:00 -0700

    created tests for create_board method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,23 @@
 #spec/board_spec.rb
+require "./lib/board.rb"
+
+describe Board do
+  describe "#create_board" do
+    it "will return an array of length 8" do
+      board = Board.new
+      expect(board.create_board.length).to eql 8
+    end
+
+    it "will contain 8 arrays of length 8" do
+      board = Board.new
+      game_board = board.create_board
+      expect(game_board.all? { |arr| arr.length == 8 }).to be true
+    end
+
+    it "second dimensional arrays should be filled with hashes" do
+      board = Board.new
+      game_board = board.create_board
+      expect(game_board.flatten.all? { |v| v.is_a?(Hash) }).to be true
+    end
+  end
+end
