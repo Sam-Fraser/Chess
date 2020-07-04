10319f213eadc1f87232ef822be28f40c5fd94b8
Sam Eff <HeySamEff@gmail.com>
2020-07-04 11:53:53 -0700

    fixed valid_move method to pass rspec tests

--- lib/board.rb
+++ lib/board.rb
@@ -33,7 +33,7 @@ class Board
 
   #check if space to move to is a valid move
   def valid_move?(arr)
-    arr[0] < 0 && arr[0] > 7 && arr[1] < 0 && arr[1] > 7 && board[arr[0]][arr[1]][:piece].nil?
+    arr[0] > 0 && arr[0] < 7 && arr[1] > 0 && arr[1] < 7 && @board[arr[0]][arr[1]][:piece].nil?
   end
 
 end
