42104be48951aafe76ce2c2bd06e7266329b66c6
Sam Eff <HeySamEff@gmail.com>
2020-07-04 15:20:37 -0700

    created second test for move method

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,14 +2,20 @@
 require "./lib/piece.rb"
 
 describe Piece do
-  describe "#move" do
-    before(:each)do
-      @w_knight = Piece.new([0,1], "w_knight")
-    end
 
+  before(:each) do
+    @w_knight = Piece.new([0,1], "w_knight")
+  end
+
+  describe "#move" do
     it "will move a piece to a new space" do
       @w_knight.move([2,1])
       expect(@w_knight.current_position).to eql [2,2]
+    end
+
+    it "will make first move false on a movement" do
+      @w_knight.move([2,1])
+      expect(@w_knight.first_move).to be false
     end
   end
 end
