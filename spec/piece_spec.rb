65177db9deb3f10d13bf451833371db9355faeca
Sam Eff <HeySamEff@gmail.com>
2020-07-05 10:40:47 -0700

    created test for movement on board functionality

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -5,8 +5,8 @@ require "./lib/board.rb"
 describe Piece do
 
   before(:each) do
-    board = Board.new
-    @w_knight = Piece.new([0,1], "w_knight", board)
+    @board = Board.new
+    @w_knight = Piece.new([0,1], "w_knight", @board)
   end
 
   describe "#move" do
@@ -18,6 +18,22 @@ describe Piece do
     it "will make first move false on a movement" do
       @w_knight.move([2,1])
       expect(@w_knight.first_move).to be false
+    end
+
+    it "will remove a piece from one space and place it in another" do
+      DISPLAY3 = <<-DISPLAY
+  a b c d e f g h 
+1 \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} 
+2 \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} 
+3 \u{25A0} \u{25A1} \u{2658} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} 
+4 \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} 
+5 \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} 
+6 \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} 
+7 \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} 
+8 \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} \u{25A1} \u{25A0} 
+      DISPLAY
+      @w_knight.move([2,1])
+      expect{ puts @board.display_board }.to output(DISPLAY3).to_stdout
     end
   end
 end
