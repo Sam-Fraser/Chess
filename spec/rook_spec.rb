7823da37f44631cbf9ed9fe982367d1ba16d3937
Sam Eff <HeySamEff@gmail.com>
2020-07-06 15:32:21 -0700

    created tests for all possible move patterns

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -8,9 +8,27 @@ describe Rook do
     @rook = Rook.new([7,0], "w_rook", @board)
   end
 
-  describe "#all_moves" do
-    it "will return all possible moves a rook can do" do
-      expect(@rook.all_moves).to eql [[0,1], [1,0], [0,-1], [-1,0], [0,2], [2,0], [0,-2], [-2,0], [0,3], [3,0], [0,-3], [-3,0], [0,4], [4,0], [0,-4], [-4,0], [0,5], [5,0], [0,-5], [-5,0], [0,6], [6,0], [0,-6], [-6,0], [0,7], [7,0], [0,-7], [-7,0]]
+  describe "#right_moves" do
+    it "will return all possible moves a rook can do to the right" do
+      expect(@rook.right_moves).to eql [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]
+    end
+  end
+
+  describe "#LEFT_MOVES" do
+    it "will return all possible moves a rook can do to the left" do
+      expect(@rook.LEFT_MOVES).to eql [[0, -1], [0, -2], [0, -3], [0, -4], [0, -5], [0, -6], [0, -7]]
+    end
+  end
+
+  describe "#UP_MOVES" do
+    it "will return all possible moves a rook can do up" do
+      expect(@rook.UP_MOVES).to eql [[-1, 0], [-2, 0], [-3, 0], [-4, 0], [-5, 0], [-6, 0], [-7, 0]]
+    end
+  end
+
+  describe "#DOWN_MOVES" do
+    it "will return all possible moves a rook can do down" do
+      expect(@rook.DOWN_MOVES).to eql [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]
     end
   end
 end
