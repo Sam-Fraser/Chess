8d79353a378cf56707d2c73762b977a73c3cf521
Sam Eff <HeySamEff@gmail.com>
2020-07-06 15:32:04 -0700

    updated initialize and right_moves method

--- lib/rook.rb
+++ lib/rook.rb
@@ -3,14 +3,28 @@ require_relative "piece.rb"
 
 #rook class (subclass of piece)
 class Rook < Piece
+  attr_reader :LEFT_MOVES, :UP_MOVES, :DOWN_MOVES
+
+  def initialize(starting_position, name, board)
+    super
+    @RIGHT_MOVES = right_moves
+    @LEFT_MOVES = @RIGHT_MOVES.map { |arr| arr = [0, -arr[1]] }
+    @UP_MOVES = @LEFT_MOVES.map { |arr| arr.reverse }
+    @DOWN_MOVES = @RIGHT_MOVES.map { |arr| arr.reverse }
+  end
   #set_moves method
-  def all_moves
+  def right_moves
     final_array = []
     7.times do |i|
-      array1 = [0,i+1]
-      array2 = [0,-i-1]
-      final_array.push(array1, array1.reverse, array2, array2.reverse)
+      array = [0,i+1]
+      final_array.push(array)
     end
-    final_array
+    final_array.sort
+  end
+
+  #possible moves method
+  def get_possible_moves
+    possible_moves = []
+    
   end
 end
