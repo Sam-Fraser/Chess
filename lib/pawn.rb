a489360180b29fbb4dd75afa807a0078e0558090
Sam Eff <HeySamEff@gmail.com>
2020-07-06 12:30:26 -0700

    created get_possible_moves method to return all possible moves

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -3,9 +3,23 @@ require_relative "piece.rb"
 
 #pawn class (subclass of piece)
 class Pawn < Piece
-  INIT_MOVES = [[-2,0],[-1,0]]
-  MOVES = [[-1,0]]
+  INIT_MOVES = [[-1,0],[-2,0]]
+  MOVES = [-1,0]
   ATTACKS = [[-1,1],[-1,-1]]
 
-  #
+  #get possible moves
+  def get_possible_moves
+    possible_moves = Array.new
+    if first_move
+      INIT_MOVES.each do |arr|
+        @board.valid_move?([@current_position[0] + arr[0], @current_position[1] + arr[1]]) ? possible_moves.push(arr) : break 
+      end
+    else
+      possible_moves.push(MOVES) if @board.valid_move?([@current_position[0] + MOVES[0], @current_position[1] + MOVES[1]])
+    end
+    ATTACKS.each do |arr| 
+      @board.valid_attack?([@current_position[0] + arr[0], @current_position[1] + arr[1]]) ? possible_moves.push(arr) : next 
+    end
+    possible_moves
+  end
 end
