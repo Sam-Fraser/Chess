8e22e6008a957be3bb444db6050bf2b417d014ee
Sam Eff <HeySamEff@gmail.com>
2020-07-07 11:40:07 -0700

    created knight.rb and spec

---
+++ lib/knight.rb
@@ -0,0 +1,9 @@
+#lib/knight.rb
+require_relative "piece.rb"
+
+#knight class (subclass of piece)
+class Knight
+  MOVES = [[2,1], [2,-1], [-2,1], [-2,-1], [1,2], [1,-2], [-1,2], [-1,-2]]
+
+  
+end
---
+++ spec/knight_spec.rb
@@ -0,0 +1,3 @@
+#spec/knight_spec.rb
+require "./lib/knight.rb"
+require "./lib/board.rb"
