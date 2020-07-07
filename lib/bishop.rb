48056099f8717a9a24f9b6aff34035c6065158c0
Sam Eff <HeySamEff@gmail.com>
2020-07-07 12:41:59 -0700

    created get_possible_moves method for bishop and created/fixed tests

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -9,7 +9,7 @@ class Bishop < Piece
     super
     @UP_RIGHT_MOVES = up_right_moves
     @UP_LEFT_MOVES = @UP_RIGHT_MOVES.map { |arr| arr = [arr[0], -arr[1]] }
-    @DOWN_RIGHT_MOVES = @UP_LEFT_MOVES.map { |arr| arr.reverse }
+    @DOWN_RIGHT_MOVES = @UP_RIGHT_MOVES.map { |arr| arr = [-arr[0], arr[1]] }
     @DOWN_LEFT_MOVES = @UP_RIGHT_MOVES.map { |arr| arr = [-arr[0], -arr[1]] }
   end
 
@@ -17,12 +17,24 @@ class Bishop < Piece
   def up_right_moves
     final_array = []
     7.times do |i|
-      array = [i+1,i+1]
+      array = [-i-1,i+1]
       final_array.push(array)
     end
-    final_array.sort
+    final_array
   end
 
   #get possible moves method (per direction)
-  
+  def get_possible_moves(array)
+    possible_moves = []
+    array.each do |arr|
+      if @board.valid_move?([@current_position[0] + arr[0], @current_position[1] + arr[1]])
+        possible_moves.push(arr)
+      elsif @board.valid_attack?([@current_position[0] + arr[0], @current_position[1] + arr[1]])
+        possible_moves.push(arr)
+        break
+      end
+    end
+    possible_moves
+  end
+
 end
--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -9,26 +9,33 @@ describe Bishop do
   end
 
   describe "#up_right_moves" do
-    it "will return all possible moves a rook can do up and to the right" do
-      expect(@bishop.up_right_moves).to eql [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]]
+    it "will return all possible moves a bishop can do up and to the right" do
+      expect(@bishop.up_right_moves).to eql [[-1, 1], [-2, 2], [-3, 3], [-4, 4], [-5, 5], [-6, 6], [-7, 7]]
     end
   end
 
   describe "#UP_LEFT_MOVES" do
-    it "will return all possible moves a rook can do up and to the left" do
-      expect(@bishop.UP_LEFT_MOVES).to eql [[1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7]]
+    it "will return all possible moves a bishop can do up and to the left" do
+      expect(@bishop.UP_LEFT_MOVES).to eql [[-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7]]
     end
   end
 
   describe "#DOWN_RIGHT_MOVES" do
-    it "will return all possible moves a rook can do down and to the right" do
-      expect(@bishop.DOWN_RIGHT_MOVES).to eql [[-1, 1], [-2, 2], [-3, 3], [-4, 4], [-5, 5], [-6, 6], [-7, 7]]
+    it "will return all possible moves a bishop can do down and to the right" do
+      expect(@bishop.DOWN_RIGHT_MOVES).to eql [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]]
     end
   end
 
   describe "#DOWN_LEFT_MOVES" do
-    it "will return all possible moves a rook can do down and to the left" do
-      expect(@bishop.DOWN_LEFT_MOVES).to eql [[-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7]]
+    it "will return all possible moves a bishop can do down and to the left" do
+      expect(@bishop.DOWN_LEFT_MOVES).to eql [[1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7]]
+    end
+  end
+
+  describe "#get_possible_moves" do
+    it "will return all possible moves a bishop can do in a certain direction, given a certain board" do
+      @board.place_piece([4,5], @board.get_symbol("b_rook"))
+      expect(@bishop.get_possible_moves(@bishop.UP_RIGHT_MOVES)).to eql [[-1, 1], [-2, 2], [-3, 3]]
     end
   end
 end
