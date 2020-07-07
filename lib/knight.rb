a966a59e15fd7c769660354108d0fbf7681a09c6
Sam Eff <HeySamEff@gmail.com>
2020-07-07 11:46:33 -0700

    created get_all_possible_moves method for knight

--- lib/knight.rb
+++ lib/knight.rb
@@ -5,5 +5,13 @@ require_relative "piece.rb"
 class Knight
   MOVES = [[2,1], [2,-1], [-2,1], [-2,-1], [1,2], [1,-2], [-1,2], [-1,-2]]
 
-  
+  #get all possible moves method
+  def get_all_possible_moves
+    possible_moves = []
+    MOVES.each do |arr|
+      move = [@current_position[0] + arr[0], @current_position[1] + arr[1]]
+      possible_moves.push(arr) if @board.valid_move?(move) || @board.valid_attack?(move)
+    end
+    possible_moves
+  end
 end
