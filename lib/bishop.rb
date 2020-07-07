3c2aa093af208889e3f3f04fc4e86c53688f21c1
Sam Eff <HeySamEff@gmail.com>
2020-07-07 12:07:05 -0700

    created up_right_moves method for bishop

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -4,4 +4,21 @@ require_relative "piece.rb"
 #bishop class (subclass of piece)
 class Bishop < Piece
 
+  def initialize(starting_position, name, board)
+    super
+    @UP_RIGHT_MOVES = up_right_moves
+    @UP_LEFT_MOVES = @RIGHT_MOVES.map { |arr| arr = [0, -arr[1]] }
+    @DOWN_RIGHT_MOVES = @LEFT_MOVES.map { |arr| arr.reverse }
+    @DOWN_LEFT_MOVES = @RIGHT_MOVES.map { |arr| arr.reverse }
+  end
+
+  #set_moves method
+  def up_right_moves
+    final_array = []
+    7.times do |i|
+      array = [i+1,i+1]
+      final_array.push(array)
+    end
+    final_array.sort
+  end
 end
